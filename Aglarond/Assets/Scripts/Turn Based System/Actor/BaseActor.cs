cf4b609c2af971af55874b4e1fa763a71afeb14b
Guilherme Oliveira <guilherme.061@gmail.com>
2019-09-02 19:43:00 -0400

    updating unity version

---
+++ Aglarond/Assets/Scripts/Turn Based System/Actor/BaseActor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO Have a reference to Turn Based System Manager?
+
+namespace FourthDimension.TurnBased.Actor {
+    public enum EActorType {
+        Player,
+        Enemy,
+        // Moving Hazard?
+        // Static Actor?
+    }
+
+    public abstract class BaseActor : MonoBehaviour {
+        protected Vector2 m_currentPosition;
+        public Vector2 CurrentPosition {
+            get {
+                return this.m_currentPosition;
+            }
+
+            set {
+                this.m_currentPosition = value;
+            }
+        }
+
+        protected EActorType m_actorType;
+        public EActorType ActorType {
+            get {
+                return this.m_actorType;
+            }
+
+            set {
+                this.m_actorType = value;
+            }
+        }
+
+        public virtual void InitializeActor(EActorType _actorType) {
+            m_currentPosition = new Vector2(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+            m_actorType = _actorType;
+        }
+
+        public abstract bool HasTakenTurn();
+        // TODO HasTakenEnforcedTurn?
+        // TODO PlayAudioClip?
+    }
+}
---
+++ Aglarond/Assets/Scripts/Turn Based System/Actor/DynamicActorComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FourthDimension.TurnBased.Actor {
+    public class DynamicActorComponent : BaseActor {
+        // TODO Stats?
+        // TODO Collision Handling?
+        // TODO Sounds?
+        // TODO Particles?
+        // TODO Death?
+        
+        // TODO Movement
+
+        // TODO Implement Has Taken Turn
+        public override bool HasTakenTurn() {
+            return true;
+        }
+    }
+}
