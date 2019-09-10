529684a671032f4b8b215203ef8dc41f1ab7210a
Guilherme Oliveira <guilherme.061@gmail.com>
2019-09-10 13:15:52 -0400

    starting turn based and dungeon integration

---
+++ Aglarond/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using FourthDimension.TurnBased.Actor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FourthDimension.CameraUtilities {
+    public class CameraFollow : MonoBehaviour {
+        private Hero m_playerReference;
+
+        private void Start() {
+            m_playerReference = GameObject.FindGameObjectWithTag("Player").GetComponent<Hero>();
+        }
+
+        private void Update() {
+            transform.position = new Vector3(m_playerReference.CurrentPosition.x, m_playerReference.CurrentPosition.y, transform.position.z);
+        }
+    }
+}
