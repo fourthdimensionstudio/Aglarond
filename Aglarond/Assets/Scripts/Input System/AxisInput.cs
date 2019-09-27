f445665c16b2ca98cbfd88448c9e9235e5be2a3b
Guilherme Oliveira <guilherme.061@gmail.com>
2019-09-26 22:20:04 -0400

    small changes on bat, input and wall tile

--- Aglarond/Assets/Scripts/Input System/AxisInput.cs
+++ Aglarond/Assets/Scripts/Input System/AxisInput.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace FourthDimension.Input {
     public class AxisInput : BaseInput {
-        private const float km_holdingTimeToAct = 0.1f;
+        private const float km_holdingTimeToAct = 0.25f;
         private float m_holdingInputTime;
 
         public AxisInput() {
